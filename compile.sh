b09e2b26628c12e2569cc4737305ee4ab0fa8de1
hentai-chan <70948244+hentai-chan@users.noreply.github.com>
2021-06-12 22:38:11 +0200

    Paramterize compile script

--- compile.sh
+++ compile.sh
@@ -1,8 +1,12 @@
+#!/usr/bin/env bash
+
 cd src/
-pdflatex document.tex
-bibtex document
-pdflatex document.tex
-pdflatex document.tex
-makeglossaries document
-pdflatex document
+
+pdflatex "${1}.tex"
+bibtex $1
+pdflatex "${1}.tex"
+pdflatex "${1}.tex"
+makeglossaries $1
+pdflatex $1
+
 cd ..
